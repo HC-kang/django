9296b9337db087e35418e11ce9dad17535a11545
Ford <weston0713@gmail.com>
2022-05-17 08:48:42 +0900

    add answers

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Question, Answer
 
 
 def index(request):
-    return HttpResponse("안녕하세요 Sherry 욕하지 마세요. 마음 착하게 쓰세요.")
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
+
+
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get(
+        'content'), create_date=timezone.now())
+    return redirect('pybo:detail', question_id=question.id)
+
+# def answer_create(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
+#     answer.save()
+#     return redirect('pybo:detail', question_id=question.id)
