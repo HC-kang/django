51736f858bd5be7f6b42549177d5f6cfc2aed1a4
Ford <weston0713@gmail.com>
2022-05-18 14:39:24 +0900

    login, logout

--- mysite/config/urls.py
+++ mysite/config/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from pybo import views
 
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
+    path('common/', include('common.urls')),
 ]
