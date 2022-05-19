b202622277bd85551ec221e3287d5c8b8b8eff56
Ford <weston0713@gmail.com>
2022-05-19 13:03:58 +0900

    수정/삭제 추가

--- mysite/common/urls.py
+++ mysite/common/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'common'
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup/', views.signup, name='signup'),
 ]
