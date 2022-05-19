49a1a219f33e2f70b7f9dacbe88b89d1d9576cd7
Ford <weston0713@gmail.com>
2022-05-19 20:48:48 +0900

    prod update

--- mysite/pybo/views/base_views.py
+++ mysite/pybo/views/base_views.py
@@ -4,7 +4,12 @@ from django.db.models import Q
 
 from ..models import Question
 
+import logging
+logger = logging.getLogger('pybo')
+
+
 def index(request):
+    logger.info('INFO 레벨로 출력')
     page = request.GET.get('page', '1')
     kw = request.GET.get('kw', '')
     question_list = Question.objects.order_by('-create_date')
