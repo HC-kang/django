c4b0d84d2d247572d8e1a91a14e441823825bdc3
Ford <weston0713@gmail.com>
2022-05-20 14:16:28 +0900

    prod update

--- mysite/config/settings/prod.py
+++ mysite/config/settings/prod.py
@@ -1,6 +1,22 @@
+import environ
+
 from .base import *
 
 ALLOWED_HOSTS = ['13.209.58.95', 'ford-camp.shop']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
+
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / '.env')
+
+DATABASES ={
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': '5432',
+    }
+}
